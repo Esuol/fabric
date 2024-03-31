45cec4271d29bd1df9e227d77ae80497665f0941
berlinen <1292393108@qq.com>
2024-03-31 20:13:07 +0800

    feat: map_type

--- codegen/src/interface.rs
+++ codegen/src/interface.rs
@@ -74,3 +74,79 @@ fn pointer_type(mutability: Option<Token![mut]>, ty: Type) -> TypePtr {
         elem: Box::new(ty),
     }
 }
+
+fn map_type(input: &Type) -> Type {
+    match input {
+        Type::Reference(reference) => match &*reference.elem {
+            Type::Path(path) => {
+                if let Some(seg) = path.path.segments.last() {
+                    match &seg.ident.to_string() as &str {
+                        "CStr" => {
+                            return Type::Ptr(pointer_type(
+                                reference.mutability.clone(),
+                                Type::Path(path_type(vec![
+                                    segment(ident("std"), None),
+                                    segment(ident("os"), None),
+                                    segment(ident("raw"), None),
+                                    segment(ident("c_char"), None),
+                                ])),
+                            ));
+                        }
+                        _ => {}
+                    }
+                }
+            }
+
+            Type::TraitObject(_) => {
+                return Type::Ptr(pointer_type(
+                    reference.mutability.clone(),
+                    Type::Path(path_type(vec![
+                        segment(ident("std"), None),
+                        segment(ident("ffi"), None),
+                        segment(ident("c_void"), None),
+                    ])),
+                ))
+            }
+
+            _ => {}
+        },
+
+        Type::Path(path) => {
+            if let Some(seg) = path.path.segments.last() {
+                match &seg.ident.to_string() as &str {
+                    "Box" => {
+                        let args = match &seg.arguments {
+                            PathArguments::AngleBracketed(args) => args,
+                            other => panic!("{:?}", other),
+                        };
+
+                        let arg = match &args.args[0] {
+                            GenericArgument::Type(arg) => arg,
+                            other => panic!("{:?}", other),
+                        };
+
+                        match arg {
+                            Type::TraitObject(_) => {
+                                return Type::Ptr(pointer_type(
+                                    Some(Token![mut](Span::call_site())),
+                                    Type::Path(path_type(vec![
+                                        segment(ident("std"), None),
+                                        segment(ident("ffi"), None),
+                                        segment(ident("c_void"), None),
+                                    ])),
+                                ));
+                            }
+
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        _ => {}
+    }
+
+    input.clone()
+}
